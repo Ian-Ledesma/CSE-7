afb131ba95f213b3f27f00955cbe3cc9f8149929
Ian-Ledesma <31050483+Ian-Ledesma@users.noreply.github.com>
2017-11-28 15:07:16 -0800

    Adding Previous Notes
    11/27/17

---
+++ Notes.py
@@ -0,0 +1,33 @@
+print('Hello World')
+
+# Ian Ledesma (Use ctrl +/ to toggle comments)
+
+print(3 + 5)
+print(5 - 3)
+print(5 * 3)
+print(6 / 2)
+print(3 ** 2)
+
+print()
+print('See if you can figure this out')
+print(20 % 12 )
+
+#Variable
+car_name = "Weibe Mobile"
+car_type = "Lamborghini Sesto Elemento"
+car_cylinder = 8
+car_mpg = 9000.1
+
+#Inline Printing
+print("My car is the %s." % car_name)
+print("My car is the %s. It is a %s" % (car_name, car_type))
+
+#Taking Input
+name = input("What is your name?")
+print("Hello %s." % name)
+print(name)
+
+age = input("What is your age?")
+print("That's nice, %s." % age)
+print(age)
+
