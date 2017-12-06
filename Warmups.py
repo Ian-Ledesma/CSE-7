785dc44252261d3c80a1654f29f2166b7be44238
Ian-Ledesma <31050483+Ian-Ledesma@users.noreply.github.com>
2017-12-06 15:02:03 -0800

    12/6/17

--- Warmups.py
+++ Warmups.py
@@ -1,34 +1,40 @@
-# # 12.4.17
+# # # 12.4.17
+# #
+# #
+# # TakingInput:(function)
+# # def reverse_order (first_name, last_name):
+# #     print(('%s %s' % (last_name, first_name))
+# #     print(last_name + "" + first_name) #Concatenation
+# #
+# # # def reverse_order():
+# # #
+# # #     first_name = input('What is your first name?')
+# # #
+# # #     last_name = input('What is your surname')
+# # #
+# # #     print('%s %s' % (last_name, first_name))
 #
 #
-# TakingInput:(function)
-# def reverse_order (first_name, last_name):
-#     print(('%s %s' % (last_name, first_name))
-#     print(last_name + "" + first_name) #Concatenation
+# # def happy_bday(name):
+# #     print("Happy birthday to you!")
+# #     print("Happy birthday to you!")
+# #     print("Happy birthday dear %s" % name)
+# #     print("Happy Birthday to you!")
 #
-# # def reverse_order():
-# #
-# #     first_name = input('What is your first name?')
-# #
-# #     last_name = input('What is your surname')
-# #
-# #     print('%s %s' % (last_name, first_name))
+#
+#
+# #12.5.17
+#
+# def add_py(name):
+#     print("%s .py" % name)
+#     print(name + ".py")
+#
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-# def happy_bday(name):
-#     print("Happy birthday to you!")
-#     print("Happy birthday to you!")
-#     print("Happy birthday dear %s" % name)
-#     print("Happy Birthday to you!")
+#12.6.17
 
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def add(num1, num2 ,num3):
+    print (num1 + num2 + num3)
 
-#12.5.17
 
-def add_py(name):
-    print("%s .py" % name)
-    print(name + ".py")
-
+add(67,1155,47)
