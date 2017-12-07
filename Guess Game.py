ca4cfa78907bca627a08e5d9802862db5410533e
Ian-Ledesma <31050483+Ian-Ledesma@users.noreply.github.com>
2017-12-07 15:42:06 -0800

    12/7/17
    inequality

--- Guess Game.py
+++ Guess Game.py
@@ -2,10 +2,13 @@ import random
 num = (random.randint(0, 51))
 print(num)
 
+guess = input("Pick a number between 0 and 50")
 
-
-
-
+while guess != num:
+    if int(guess) > int(num):
+        print("Come now, a bit HIGHER")
+    if int(guess) < int(num):
+        print("Woah there! a bit LOWER.")
 
 # Get a number
 # Get a number (input) from the user
--- Warmups.py
+++ Warmups.py
@@ -1,40 +1,47 @@
-# # # 12.4.17
+# # # # # 12.4.17
+# # # #
+# # # #
+# # # # TakingInput:(function)
+# # # # def reverse_order (first_name, last_name):
+# # # #     print(('%s %s' % (last_name, first_name))
+# # # #     print(last_name + "" + first_name) #Concatenation
+# # # #
+# # # # # def reverse_order():
+# # # # #
+# # # # #     first_name = input('What is your first name?')
+# # # # #
+# # # # #     last_name = input('What is your surname')
+# # # # #
+# # # # #     print('%s %s' % (last_name, first_name))
+# # #
+# # #
+# # # # def happy_bday(name):
+# # # #     print("Happy birthday to you!")
+# # # #     print("Happy birthday to you!")
+# # # #     print("Happy birthday dear %s" % name)
+# # # #     print("Happy Birthday to you!")
+# # #
+# # #
+# # #
+# # # #12.5.17
+# # #
+# # # def add_py(name):
+# # #     print("%s .py" % name)
+# # #     print(name + ".py")
+# # #
+# #
+# # # 12.6.17
 # #
 # #
-# # TakingInput:(function)
-# # def reverse_order (first_name, last_name):
-# #     print(('%s %s' % (last_name, first_name))
-# #     print(last_name + "" + first_name) #Concatenation
+# # def add(num1, num2, num3):
+# #     print(num1 + num2 + num3)
 # #
-# # # def reverse_order():
-# # #
-# # #     first_name = input('What is your first name?')
-# # #
-# # #     last_name = input('What is your surname')
-# # #
-# # #     print('%s %s' % (last_name, first_name))
+# #
+# # add(69, 666, 420)
 #
 #
-# # def happy_bday(name):
-# #     print("Happy birthday to you!")
-# #     print("Happy birthday to you!")
-# #     print("Happy birthday dear %s" % name)
-# #     print("Happy Birthday to you!")
+# # 12.7.16
 #
-#
-#
-# #12.5.17
-#
-# def add_py(name):
-#     print("%s .py" % name)
-#     print(name + ".py")
-#
-
-# 12.6.17
-
-
-def add(num1, num2, num3):
-    print(num1 + num2 + num3)
-
-
-add(69, 666, 420)
+# def repeat(string):
+#     for x in range(3):
+#         print(string)
