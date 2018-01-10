1da4840478617acff7e822956beb644a7034f4ec
Ian-Ledesma <31050483+Ian-Ledesma@users.noreply.github.com>
2018-01-10 15:41:24 -0800

    1/10/17
    Start loop

--- Lucky 7s.py
+++ Lucky 7s.py
@@ -1,6 +1,17 @@
 import random
-die_01 = (random.randint(1,6))
-die_02 = (random.randint(1,6))
+die_01 = (random.randint(1, 6))
+die_02 = (random.randint(1, 6))
 
-dinero = 15
+money = 15
 
+roll = die_01 + die_02
+
+while money == 0:
+    if money != 0:
+        money -= 1
+    if roll == 7:
+        money += 5
+    if roll != 7:
+        money -= 1
+
+print("You've got %s dollars" % money)
