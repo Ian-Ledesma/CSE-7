da908e09bb6cbd29e38391a4c7ca1ad429c628ba
Ian-Ledesma <31050483+Ian-Ledesma@users.noreply.github.com>
2018-01-11 15:39:52 -0800

    1/11/18
    Starting extension

--- Lucky 7s.py
+++ Lucky 7s.py
@@ -1,17 +1,21 @@
 import random
-die_01 = (random.randint(1, 6))
-die_02 = (random.randint(1, 6))
 
 money = 15
 
-roll = die_01 + die_02
+rounds = 0
 
-while money == 0:
-    if money != 0:
-        money -= 1
+while money > 0:
+    die_01 = (random.randint(1, 6))
+    die_02 = (random.randint(1, 6))
+    roll = (die_01 + die_02)
+
     if roll == 7:
-        money += 5
-    if roll != 7:
+        money += 4
+        rounds += 1
+    elif roll != 7:
         money -= 1
+        rounds += 1
 
 print("You've got %s dollars" % money)
+print("YOur highest amount of money is $ %s" %)
+print("You played %s rounds" % rounds)
