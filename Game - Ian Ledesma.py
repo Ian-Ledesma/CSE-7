78a5baf92401b56c52dba6d61d4d923d3746bd80
Ian-Ledesma <31050483+Ian-Ledesma@users.noreply.github.com>
2018-02-15 15:39:19 -0800

    2/15/18

---
+++ Game - Ian Ledesma.py
@@ -0,0 +1,66 @@
+Spongebob_Pineapple = {
+    'SOUTHHOUSE': {
+        'NAME': "THE PINEAPPLE'S ENTRANCE",
+        'DESCRIPTION': "BEFORE YOU IS A MASSIVE METAL DOOR. NORTH OF YOU IS THE LIVING ROOM.",
+        'PATHS': {
+            'NORTH': "NORTHHOUSE",
+            'SOUTH': "SOUTHHOUSE",
+            'EAST': "EASTHOUSE"
+        }
+    },
+    'INTROOM1': {
+        'NAME': 'LIVING ROOM',
+        'DESCRIPTION': "AHEAD OF YOU ARE TWO BALLOON SOFAS, 2 FISHHOOKS AND A PORTRAIT ON THE WALL, A HELMET FOR A TV,"
+                       "AND A BLUE BAMBOO WALL. TO THE EAST IS A CLOSET, AND TO THE NORTH IS A KITCHEN.",
+        'PATHS': {
+            'NORTH': "SOUTHHOUSE",
+            'EAST': "EASTHOUSE"
+        }
+    },
+    'SOUTHHOUSE': {
+        'NAME': 'CLOSET',
+        'DESCRIPTION': "",
+        'PATHS': {
+            'NORTH': "NORTHHOUSE",
+            'WEST': "WESTHOUSE",
+            'EAST': "EASTHOUSE"
+        }
+    },
+    'EASTHOUSE': {
+        'NAME': "GARY'S ROOM",
+        'DESCRIPTION': "YOU SEE A PINK WALLED ROOM ",
+        'PATHS': {
+            'NORTH': "NORTHHOUSE",
+            'SOUTH': "SOUTHHOUSE",
+            'EAST': "EASTHOUSE"
+        }
+    }
+    'EASTHOUSE': {
+        'NAME': "GARRY'S CONSCIOUS",
+        'DESCRIPTION': "You are west of a small house",
+        'PATHS': {
+            'NORTH': "NORTHHOUSE",
+            'SOUTH': "SOUTHHOUSE",
+            'EAST': "EASTHOUSE"
+        }
+    }
+    'EASTHOUSE': {
+        'NAME': "CELLAR",
+        'DESCRIPTION': "You are west of a small house",
+        'PATHS': {
+            'NORTH': "NORTHHOUSE",
+            'SOUTH': "SOUTHHOUSE",
+            'EAST': "EASTHOUSE"
+        }
+    }
+    'EASTHOUSE': {
+        'NAME': "BEDROOM",
+        'DESCRIPTION': "You are west of a small house",
+        'PATHS': {
+            'NORTH': "NORTHHOUSE",
+            'SOUTH': "SOUTHHOUSE",
+            'EAST': "EASTHOUSE"
+        }
+    }
+}
+
