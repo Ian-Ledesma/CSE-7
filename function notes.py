ac7d6a49bbef173fbb08df27ce674dc2af7907fa
Ian-Ledesma <31050483+Ian-Ledesma@users.noreply.github.com>
2018-02-20 15:42:36 -0800

    2/20/18

---
+++ function notes.py
@@ -0,0 +1,42 @@
+# Defining Functions
+def hello_world():
+    print("Hello World!")
+
+
+hello_world()
+
+
+def square_it(number):
+    return number ** 2
+
+
+print(square_it(3))
+
+for i in range(5):
+    print(square_it(i))
+
+
+def tip_calc(subtotal):
+    tip_amt = subtotal * 0.18  # tip_at is a local variable
+    print("The tip amount is $ %s " %tip_amt)
+    return tip_amt
+
+
+def total_bill(subtotal):
+    total = subtotal + tip_calc(subtotal)
+    return total
+
+
+print(total_bill(100))
+
+
+def distance(x1, y1, x2, y2):
+    inside = (x2 - x1) ** 2 + (y2 - y1) ** 2
+    answer = inside ** 0.5
+    return answer
+
+
+print(distance(0, 0, 3, 4))
+
+
+def pythagorean()
---
+++ pythagorean theorem.py
@@ -0,0 +1,9 @@
+def pythagorean(a, b):
+    a = input("a squared")
+    b = input("b squared")
+
+    c_squared = a ** 2 + b ** 2
+    print(pythagorean(a, b))
+
+    print(pythagorean(5, 12)) == 13
+
