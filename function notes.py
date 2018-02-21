b6d98f66b1b7f63a532e27ee2c8f33c4e98b6f1f
Ian-Ledesma <31050483+Ian-Ledesma@users.noreply.github.com>
2018-02-21 15:45:48 -0800

    2/21/18

--- function notes.py
+++ function notes.py
@@ -43,4 +43,69 @@
 
 #  Defining a class
 class Cat(object):
-    
+    # TWO underscores before and after
+    def __init__(self, color, pattern):
+        #  Things that a Cat has
+        self.color = color
+        self.pattern = pattern
+        self.state = "happy"
+        self.hungry = False
+
+    #  Things that a Cat can do
+    def jump(self):
+        self.state = "Scared"
+        print("The cat jumps in the air")
+
+    def play(self):
+        self.state = "happy"
+        print("You play with the cat")
+
+
+#  Instantiating (creating) two cats
+cute_cat = Cat("brown", "Spots")
+cute_cat2 = Cat("gray", "no spots")
+
+#  Getting information about the cats
+print(cute_cat.color)
+print(cute_cat2.state)
+print(cute_cat2.color)
+
+cute_cat.jump()
+print(cute_cat.state)
+print(cute_cat2.state)
+
+cute_cat.play()
+print(cute_cat.state)
+
+
+class Car(object):
+    def __init__(self, cilinders, type, color):
+        self.color = color
+        self.type = type
+        self.engineOn = False
+        self.Cilinders = cilinders
+
+    def turn_on(self):
+        if self.engineOn:
+            print("Nothing Happens")
+        else:
+            print ("The engine turns on")
+            self.engineOn = True
+
+    def move_forward(self):
+        if self.engineOn
+            print("You move froward")
+        else:
+            print("nothing happens")
+
+    def turn_off(self):
+        if self.engineOn = False:
+            print("The engine turns off")
+        else:
+            print("nothing happens")
+
+my_car = Car(4, "Ford", "Gray")
+
+my_car.turn_on()
+my_car.move_forward()
+my_car.turn_off()
