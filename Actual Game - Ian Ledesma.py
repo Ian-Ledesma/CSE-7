7103c8a319a15395a319ee5c33fa9b6699c81a4e
Ian-Ledesma <31050483+Ian-Ledesma@users.noreply.github.com>
2018-03-01 15:40:14 -0800

    3/1/18

--- Actual Game - Ian Ledesma.py
+++ Actual Game - Ian Ledesma.py
@@ -14,11 +14,33 @@ class Room(object):
         current_node = globals()[getattr(self, direction)]
 
 
-intrm1 = Room('living Room', 'intrm2', 'extrm1', None, None, None, None,
+extrm1 = Room()
+extrm2 = Room()
+extrm3 = Room()
+intrm1 = Room('Living Room', 'intrm2', 'extrm1', None, None, None, None,
               "ahead of you are two balloon sofas, 2 fishhooks and a portrait on the wall, a"
               " helmet for a tv, and a blue bamboo wall. to the east is a closet, and to the north is a kitchen.")
-extrm1 = Room
-
+intrm2 = Room('Kitchen', 'extrm1', 'intrm2', None, None, None, None, "there's a kitchen with a metal wall to one"
+            "side and bamboo on the other, as well as bamboo cuboards and surf board counters, and a bucket for a sink")
+intrm3 = Room('Kitchen Window', 'extrm3', 'intrm2', None, None, None, None, "you lean on a matal rimmed, circle window."
+              "north, outside of it is the outdoors.")
+intrm4 = Room('Closet', None, None, intrm1, intrm14, None, None, "you see a collection of party supplies, cooking "
+            "supplies, and other supplies of literally anything else. we-a-st of you is a small door, leading to"
+            "someone else's room ...")
+intrm5 = Room
+intrm6 = Room
+intrm7 = Room
+intrm8 = Room
+intrm9 = Room
+intrm10 = Room
+intrm11 = Room
+intrm12 = Room
+intrm13 = Room
+intrm14 = Room
+intrm15 = Room
+intrm16 = Room
+extrm2 = Room
+extrm3 = Room
 while True:
     print(current_node["NAME"])
     print(current_node['DESCRIPTION'])
