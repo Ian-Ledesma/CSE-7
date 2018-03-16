5f035f84f902c475c24cd33ca6253ad3fbe5accb
Ian-Ledesma <31050483+Ian-Ledesma@users.noreply.github.com>
2018-03-16 15:41:20 -0700

    3/19/18

---
+++ Item - Ian Ledesma.py
@@ -0,0 +1,10 @@
+class Items(object):
+    def __init__(self, name, weapons, key, armor, heal):
+        self.name
+        self.weapons
+        self.key
+        self.armor
+        self.heal
+
+
+    def __init__(self):
