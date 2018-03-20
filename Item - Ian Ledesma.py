a0269bdf991bb02f2cdc08f66d6d0e3adfcccfeb
Ian-Ledesma <31050483+Ian-Ledesma@users.noreply.github.com>
2018-03-20 15:39:23 -0700

    3/20

--- Item - Ian Ledesma.py
+++ Item - Ian Ledesma.py
@@ -1,24 +1,39 @@
 class Items(object):
-    def __init__(self, name, weapons, key, armor, heal):
+    def __init__(self, name, description):
         self.name = name
-        self.weapons = weapons
-        self.key = key
-        self.armor = armor
-        self.heal = heal
+        self.description = description
+        # self.weapons = weapons
+        # self.key = key
+        # self.armor = armor
+        # self.heal = heal
+#  Done
 
 
 class Key(Items):
-    def __init__(self, time_machine, clarinet):
+    def __init__(self, time_machine, clarinet, open):
+        super(Key, self).__init__(self.name, self.description, time_machine, clarinet)
         self.time_machine = time_machine
         self.clarinet = clarinet
+#  Key Done
 
 
 class Weapons(Items):
     def __init__(self, sand, kitchen_appliance, magic):
+        super(Weapons)
         self.sand = sand
         self.kitchen_appliance = kitchen_appliance
         self.magic = magic
 
+
+class Magic(Weapons):
+    def __init__(self, wand, hocus_pocus_kit, magic_conch, imagination_box):
+        super(Magic, self). __init__(self.sand, self.kitchen_appliance, self.magic, self.wand, self.hocus_pocus_kit,
+                                     self.magic_conch, self.imagination_box)
+        self.wand = wand
+        self.hocus_pocus_kit = hocus_pocus_kit
+        self.magic_conch = magic_conch
+        self.imagination_box = imagination_box
+#  Done Magic
 
 class Armor(Items):
     def __init__(self, natural, applicable):
@@ -38,8 +53,6 @@ class Spikes(Natural):
     def __init__(self, sponge_abrasion, sea_urchin_spikes, pufferfish_spikes):
         self.sponge_absorbtion = sponge_abrasion
         self. pufferfish = pufferfish_spikes
-        self.sea_urchin_spikes = sea_urchin_spikes
-
 
 
 class Heal(Items):
