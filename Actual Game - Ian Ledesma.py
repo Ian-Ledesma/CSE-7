781bf521777c9ca07efcbe66bd662815b1a1ba4d
Ian-Ledesma <31050483+Ian-Ledesma@users.noreply.github.com>
2018-04-18 15:41:11 -0700

    4/18/18
    ur mum gays

--- Actual Game - Ian Ledesma.py
+++ Actual Game - Ian Ledesma.py
@@ -35,16 +35,15 @@ class Character(object):
 extrm1 = Room('South of Pineapple', 'intrm1', "#road", None, 'extrm2', None, None, "You're near a big metal door on a"
               "pine apple.\n North is a living room, south is a road, east is sand, and west is a window into "
               "Squidward's house.")
-extrm2 = Room('West of Pineapple', None, "#road", "intrm13", "#Squidward's house", None, None, "" )
+extrm2 = Room('West of Pineapple', None, "#road", "intrm13", "#Squidward's house", None, None, "You're ne" )
 extrm3 = Room("North of Pineapple", None, 'extrm4', None, None, None, None, "Around you is sand, all but the window "
               "southward.")
-extrm4 = Room()
 intrm1 = Room('Living Room', 'intrm2', 'extrm1', None, None, None, None, "In front of you is a room lined with fishing "
               "and swimming equipment as furniture, helmet for a tv, and a blue bamboo wall.\n To the east is a closet,"
               "and to the north is a kitchen.")
-intrm2 = Room('Kitchen', 'extrm1', 'intrm2', None, None, None, None, "there's a kitchen with a metal wall to one"
+intrm2 = Room('Kitchen', 'extrm1', 'intrm2', None, None, None, None, "There's a kitchen with a metal wall to one"
               "side and bamboo on the other, as well as bamboo cuboards and surf board counters, and a bucket for a "
-              "sink")
+              "sink.\n Northward is the window, southward is the living room, eastward is another window, ")
 intrm3 = Room('Kitchen Window', 'extrm3', 'intrm2', None, None, None, None, "you lean on a metal rimmed, circle window."
               "north, outside of it is the outdoors.")
 intrm4 = Room('Closet', None, None, 'intrm1', 'intrm14', None, None, "There's a collection of supplies for literally"
@@ -59,17 +58,23 @@ intrm7 = Room('Library', None, None, None, None, 'intrm6', None, "All around you
               "middle of it all.")
 intrm8 = Room('Bedroom Window', 'extrm4', 'intrm6', None, None, None, None, "outside of the window is a street, sand" 
               "on either side for as afar as the eye can see. northward is a 2 story fall to the outer world")
-intrm9 = Room('Hallway', None, None, "intrm6", "intrm10", None, None, "you walk o" )
-intrm10 = Room('Restroom', 'intrm11', None, 'Intrm9', None, None,   )
+intrm9 = Room('Hallway', None, None, "intrm6", "intrm10", None, None, "")
+intrm10 = Room('Restroom', 'intrm11', None, 'intrm9', None, None, None, "Around you is a green, floral wall, with a tub"
+               "toilet, sink, and a wringer.\n Northward is a window.")
 intrm11 = Room('Restroom Window', 'extrm3', 'intrm10', None, None, None, None, "outside of the window is a sandy floor,"
-               "with a city far off into the distance south is a 2 story fall to the outer world")
+               "with a city far off into the distance. \n South is a 2 story fall to the outer world")
 intrm12 = Room('Kitchen Window', None, 'extrm1', None, "intrm10", None, None, "outside of the window is a street, sand"
                " on either side for as afar as the eye can see. Southward is a 2 story fall to the outer world.")
 intrm13 = Room('Garage', None, None, 'intrm2', 'extrm2', None, None, "A metal walled room, with tools one one of them"
-               "and a large, mobile door.\n Westward is the outdoors, astward is a return to the   ")
-intrm14 = Room("Gary's Mind", )
-intrm15 = Room
-intrm16 = Room()
+               "and a large, mobile door.\n Westward is the outdoors, eastward is a return to the hell of a demented "
+               "mind.")
+intrm14 = Room("Gary's Shell", None, None, "intrm4", "maze1", None, None, "You enter the pink snail shell and find a"
+               "book shelf, a fire place, a recliner. \n Westward, toward the chair, is the snail with a gaze to shake "
+               "the most mighty of warriors.")
+intrm15 = Room("Cellar", None, None, None, None, "intrm 5", None, "You see many barrels filled with root beer, from "
+               "floor to ceiling,\nwith the only way out being up.")
+intrm16 = Room("Sunroof", "extrm3", "extrm1", None, "extrm2", None, "intrm6", "Around you are some big pineapple leaves"
+               "and no ceiling.\n All around is a path to freedom, or a painful retreat downward.")
 
 #  Maze Gary
 maze1 = Room("Gary's Mind", None, None, None, "maze2", None, None, "A disgustingly colorful expansion surrounds"
